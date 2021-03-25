bfa9018ec35151df6bbf7c2a9e686fedd495acda
Aloïs <a.beurey@it-students.fr>
2021-03-25 15:24:01 +0100

    registerUdate

--- lib/components/landing.dart
+++ lib/components/landing.dart
@@ -4,167 +4,163 @@ import 'package:dwm14/screens/home.dart';
 import 'package:flutter/material.dart';
 
 class Landing extends StatelessWidget {
-  const Landing({
-    Key key,
-    this.sort= 'all'
-  }) : super(key: key);
+  const Landing({Key key, this.sort = 'all'}) : super(key: key);
 
   final String sort;
 
-
   @override
   Widget build(BuildContext context) {
-
     return FutureBuilder(
-      future: getMovies(),
-      builder: (context, snapshot) {
-            
-        switch (snapshot.connectionState) {
-          case ConnectionState.waiting: // en attente
-            return Center(
-              child: SizedBox(
-                width: 30, height: 30, child: CircularProgressIndicator()),
-            );
-          default: // Future terminée
-            if (!snapshot.hasData) {
+        future: getMovies(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting: // en attente
               return Center(
-                child: Text('No movies'),
+                child: SizedBox(
+                    width: 30, height: 30, child: CircularProgressIndicator()),
               );
-            }
-            List totalmovies = snapshot.data;
-            List movies= totalmovies;
-            if(sort != null){
-              movies= sortMovies(sort, totalmovies);
-            }
+            default: // Future terminée
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Text('No movies'),
+                );
+              }
+              List totalmovies = snapshot.data;
+              List movies = totalmovies;
+              if (sort != null) {
+                movies = sortMovies(sort, totalmovies);
+              }
 
-            return Container(
-              child: Column(
+              return Container(
+                  child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  
                   Text('Movies'),
+                  Container(height: 350, child: ListMovie(movies: movies)),
                   Container(
-                    height: 350,
-                    child: ListMovie(movies: movies)
-                  ),
-                  Container(
-                    color: Colors.red,
-                    height: 200,
-                    child: Column(
-                      children: [
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Crime'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Drama'),
+                      color: Colors.red,
+                      height: 200,
+                      child: Column(
+                        children: [
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Crime')));
+                            },
+                            child: Container(
+                              child: Text('Drama'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Drama'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Action'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Drama')));
+                            },
+                            child: Container(
+                              child: Text('Action'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Action'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Crime'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Action')));
+                            },
+                            child: Container(
+                              child: Text('Crime'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Thriller'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Thriller'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Thriller')));
+                            },
+                            child: Container(
+                              child: Text('Thriller'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Crime'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Crime'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Crime')));
+                            },
+                            child: Container(
+                              child: Text('Crime'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Fantasy'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Fantasy'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Fantasy')));
+                            },
+                            child: Container(
+                              child: Text('Fantasy'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Sci-Fi'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Sci-Fi'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Sci-Fi')));
+                            },
+                            child: Container(
+                              child: Text('Sci-Fi'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen(sort: 'Adventure'))
-                            );
-                          },
-                          child: Container(
-                            child: Text('Adventure'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          HomeScreen(sort: 'Adventure')));
+                            },
+                            child: Container(
+                              child: Text('Adventure'),
+                            ),
                           ),
-                        ),
-                        InkWell (
-                          onTap: (){
-                            Navigator.pushReplacement(
-                              context, 
-                              MaterialPageRoute(builder: (context) => HomeScreen())
-                            );
-                          },
-                          child: Container(
-                            child: Text('All'),
+                          InkWell(
+                            onTap: () {
+                              Navigator.pushReplacement(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeScreen()));
+                            },
+                            child: Container(
+                              child: Text('All'),
+                            ),
                           ),
-                        ),
-                      ],
-                    )
-                  )
+                        ],
+                      ))
                 ],
-              )
-            );
-        }
-      });
+              ));
+          }
+        });
   }
 
-  List sortMovies(String sort, List movies){
-    List sortMovies= [];
+  List sortMovies(String sort, List movies) {
+    List sortMovies = [];
     for (var movie in movies) {
-      if( movie.kind.contains(sort)) sortMovies.add(movie);
+      if (movie.kind.contains(sort)) sortMovies.add(movie);
     }
     return sortMovies;
   }
@@ -184,21 +180,15 @@ class ListMovie extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: movies.length,
       itemBuilder: (context, index) {
-
         return Padding(
-            
             padding: EdgeInsets.all(8),
             child: GestureDetector(
-              onTap: (){
-                Navigator.pushNamed(
-                  context, 
-                  '/film',
-                  arguments: ItemArguments(movie: movies[index])
-                );
+              onTap: () {
+                Navigator.pushNamed(context, '/film',
+                    arguments: ItemArguments(movie: movies[index]));
               },
               child: Column(
                 children: [
-                  
                   Container(
                     height: 220,
                     width: 130,
@@ -206,13 +196,11 @@ class ListMovie extends StatelessWidget {
                         image: DecorationImage(
                             image: NetworkImage(movies[index].poster))),
                   ),
-                  Text(
-                    movies[index].title,
-                    style: TextStyle(
-                      fontSize: 15,
-                      fontWeight: FontWeight.bold,
-                    )
-                  ),
+                  Text(movies[index].title,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      )),
                   Text(movies[index].year),
                 ],
               ),
--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -18,144 +18,148 @@ class _RegisterScreenState extends State<RegisterScreen> {
 
   Widget build(BuildContext context) {
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         body: SafeArea(
             child: Center(
                 child: Padding(
-      padding: EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          Text('Register'),
-          SizedBox(height: 10),
-          Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                TextFormField(
-                  onSaved: (value) {
-                    setState(() {
-                      firstName = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Empty';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(labelText: 'First Name'),
-                ),
-                TextFormField(
-                  onSaved: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Empty';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Name', hintText: 'Enter your name'),
-                ),
-                TextFormField(
-                  onSaved: (value) {
-                    setState(() {
-                      email = value;
-                    });
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Empty';
-                    } else if (!RegExp(r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-z]+$")
-                        .hasMatch(value)) {
-                      return "Invalid Email";
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Email', hintText: 'Enter your email'),
-                ),
-                TextFormField(
-                  obscureText: true,
-                  onSaved: (value) {
-                    password = value;
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Empty';
-                    } else if (value.length < 8) {
-                      return 'Passwort too short';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(
-                      labelText: 'Password', hintText: 'Enter your password'),
-                ),
-                TextFormField(
-                  onSaved: (value) {
-                    city = value;
-                  },
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return 'Empty';
-                    }
-                    return null;
-                  },
-                  decoration: InputDecoration(labelText: 'City'),
-                ),
-                SizedBox(height: 10),
-                Text(errorValidation),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  onPressed: () async {
-                    if (_formKey.currentState.validate()) {
-                      _formKey.currentState.save();
-                      try {
-                        UserCredential userCredential =
-                            await firebaseAuth.createUserWithEmailAndPassword(
-                                email: email, password: password);
-                        if (userCredential.user != null) {
-                          final docReference = firestore.collection('users');
-                          docReference.add({
-                            'name': name,
-                            'firstName': firstName,
-                            'email': userCredential.user.email,
-                            'city': city
-                          }).catchError((error) {
-                            setState(() {
-                              errorValidation =
-                                  'Failed to add your credentials';
-                            });
-                          });
-                        }
+          padding: EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              Text('Register'),
+              SizedBox(height: 10),
+              Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      onSaved: (value) {
                         setState(() {
-                          Navigator.pushReplacementNamed(context, '/home');
+                          firstName = value;
                         });
-                        print('Succeed to register');
-                      } on FirebaseAuthException catch (error) {
-                        if (error.code == 'weak-password') {
-                          setState(() {
-                            errorValidation = 'Password too weak';
-                          });
-                        } else if (error.code == 'email-already-in-use') {
-                          setState(() {
-                            errorValidation = 'Email already used';
-                          });
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Empty';
                         }
-                      } catch (error) {
-                        print(error);
-                      }
-                    }
-                  },
-                  child: Text('Submit'),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
-    ))));
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: 'First Name'),
+                    ),
+                    TextFormField(
+                      onSaved: (value) {
+                        setState(() {
+                          name = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Empty';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Name', hintText: 'Enter your name'),
+                    ),
+                    TextFormField(
+                      onSaved: (value) {
+                        setState(() {
+                          email = value;
+                        });
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Empty';
+                        } else if (!RegExp(
+                                r"^[a-zA-Z0-9.]+@[a-zA-Z0-9]+\.[a-z]+$")
+                            .hasMatch(value)) {
+                          return "Invalid Email";
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Email', hintText: 'Enter your email'),
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      onSaved: (value) {
+                        password = value;
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Empty';
+                        } else if (value.length < 8) {
+                          return 'Passwort too short';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          hintText: 'Enter your password'),
+                    ),
+                    TextFormField(
+                      onSaved: (value) {
+                        city = value;
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Empty';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(labelText: 'City'),
+                    ),
+                    SizedBox(height: 10),
+                    Text(errorValidation),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          _formKey.currentState.save();
+                          try {
+                            UserCredential userCredential = await firebaseAuth
+                                .createUserWithEmailAndPassword(
+                                    email: email, password: password);
+                            if (userCredential.user != null) {
+                              final docReference =
+                                  firestore.collection('users');
+                              docReference.add({
+                                'name': name,
+                                'firstName': firstName,
+                                'email': userCredential.user.email,
+                                'city': city
+                              }).catchError((error) {
+                                setState(() {
+                                  errorValidation =
+                                      'Failed to add your credentials';
+                                });
+                              });
+                            }
+                            setState(() {
+                              Navigator.pushReplacementNamed(context, '/home');
+                            });
+                            print('Succeed to register');
+                          } on FirebaseAuthException catch (error) {
+                            if (error.code == 'weak-password') {
+                              setState(() {
+                                errorValidation = 'Password too weak';
+                              });
+                            } else if (error.code == 'email-already-in-use') {
+                              setState(() {
+                                errorValidation = 'Email already used';
+                              });
+                            }
+                          } catch (error) {
+                            print(error);
+                          }
+                        }
+                      },
+                      child: Text('Submit'),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ))));
   }
 }
